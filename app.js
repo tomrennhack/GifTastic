cfd76978c7d27d0ec7c43c413728b5f83bb36510
tomrennhack <tomrennhack@gmail.com>
2019-08-29 17:36:50 -0400

    fixed gif animate

--- app.js
+++ app.js
@@ -3,33 +3,40 @@
 // VARIABLES
 
 var topics = [
-    "Dodge Viper", 
+    "Dodge Viper",
     "Corvette",
     "Ferrari",
     "Maserati",
     "Lamborghini",
-    "Camaro"
+    "Camaro",
+    "Ford Mustang",
+    "Chevelle SS",
+    "Bugatti",
+    "Volvo SC30",
+    "Mini Cooper",
+    "Mercedes AMG",
+    "BMW M5"    
 ];
+
 
 // FUNCTIONS
 
-// switch betweeen still and animated GIF
-function playPauseGif (gifItem) {
-    var state = $(gifItem).attr("data-state"); // var to capture data state
-    
-    // check current state of GIF and animate or make still, will be activated by a click
-    if (state === "still") {
-        var animateURL = $(gifItem).attr("data-animate");
-        $(gifItem).attr("src", animateURL);
-        $(gifItem).attr("data-state", "animate");
-    } else {
-        var stillImageURL = $(gifItem).attr("data-still");
-        $(gifItem).attr("src", stillImageURL);
-        $(gifItem).attr("data-state", "still");
+// switch between still and animated gifs
+function changeState(gifItem) {
+    var state = $(gifItem).attr("data-state"); // var to capture data-state
+
+    if (state === "still") { // if still
+        var animateURL = $(gifItem).attr("data-animate"); // var for animate URL
+        $(gifItem).attr("src", animateURL); // set src to animateURL
+        $(gifItem).attr("data-state", "animate"); // set data-state to animate
+    } else { // if animated
+        var stillImageURL = $(gifItem).attr("data-still"); // var for still image URL 
+        $(gifItem).attr("src", stillImageURL); // set src to stillImageURL
+        $(gifItem).attr("data-state", "still"); // set data-state to still
     }
 }
 
-// function for pulling from GIPHY
+// function to display gifs
 function displayCarGifs() {
     var sportsCar = $(this).attr("data-name"); // store data name from the button that is clicked
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -41,25 +48,26 @@ function displayCarGifs() {
     })
         .then(function (response) { // After data comes back from the request
             var results = response.data; // storing the data from the AJAX request in the results variable
+
             for (var i = 0; i < results.length; i++) { // looping through each result item
                 var sportsCarDiv = $("<div>"); // create div tag for each sportsCar
+                $("#gifs-view").prepend(sportsCarDiv); // prepend sportsCarDiv to the gifs page area
+
                 var pRating = $("<p>").text("Rating: " + results[i].rating.toUpperCase()); // create p-tag with rating
                 var sportsCarImage = $("<img>").addClass("gif mr-4").attr({ // create img tag with class, margin, and attributes
-                    "src": results["images"]["fixed_height_still"]["url"], // original src should be still image
-                    "data-still": results["images"]["fixed_height_still"]["url"], // attr for still image URL
-                    "data-animate": results["images"]["fixed_height"]["url"], // attr for animated image URL
-                    "data-state": "still" // default data-state set to still
+                    "src": results[i]["images"]["fixed_height_still"]["url"], // attr for original src - still image URL
+                    "data-still": results[i]["images"]["fixed_height_still"]["url"], // attr for still image URL
+                    "data-animate": results[i]["images"]["fixed_height"]["url"], // attr for animated image URL
+                    "data-state": "still" // default data-state of still
                 });
-                
-                // recognize click
-                sportsCarImage.on("click", function(event) {
-                    playPauseGif(event.currentTarget);
-                }
-                
+
+                sportsCarImage.on("click", function (event) { // to recognize a clicked img
+                    changeState(event.currentTarget); // change state of the current target
+                });
+
                 sportsCarDiv.append(sportsCarImage); // append img to sportsCarDiv
                 sportsCarDiv.append(pRating); // append rating to sportsCarDiv
 
-                $("#gifs-view").prepend(sportsCarDiv); // prepend sportsCarDiv to the gifs page area
             }
         });
 };
@@ -87,7 +95,7 @@ $("#add-car").on("click", function (event) {
 });
 
 // function to switch between still and animated images
-$(".gif").on("click", function() {
+$(".gif").on("click", function () {
     console.log("click test");
     if (state === "still") {
         $(this).attr("src", animatedURL);
