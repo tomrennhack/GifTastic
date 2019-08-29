1cd03d1530c8837b2de74716c242eaad479fea2f
tomrennhack <47124208+tomrennhack@users.noreply.github.com>
2019-08-29 17:07:53 -0400

    click function added and called during gif create

--- app.js
+++ app.js
@@ -2,9 +2,32 @@
 
 // VARIABLES
 
-var topics = ["Dodge Viper", "Corvette", "Ferrari", "Maserati", "Lamborghini", "Camaro"];
+var topics = [
+    "Dodge Viper", 
+    "Corvette",
+    "Ferrari",
+    "Maserati",
+    "Lamborghini",
+    "Camaro"
+];
 
 // FUNCTIONS
+
+// switch betweeen still and animated GIF
+function playPauseGif (gifItem) {
+    var state = $(gifItem).attr("data-state"); // var to capture data state
+    
+    // check current state of GIF and animate or make still, will be activated by a click
+    if (state === "still") {
+        var animateURL = $(gifItem).attr("data-animate");
+        $(gifItem).attr("src", animateURL);
+        $(gifItem).attr("data-state", "animate");
+    } else {
+        var stillImageURL = $(gifItem).attr("data-still");
+        $(gifItem).attr("src", stillImageURL);
+        $(gifItem).attr("data-state", "still");
+    }
+}
 
 // function for pulling from GIPHY
 function displayCarGifs() {
@@ -21,14 +44,18 @@ function displayCarGifs() {
             for (var i = 0; i < results.length; i++) { // looping through each result item
                 var sportsCarDiv = $("<div>"); // create div tag for each sportsCar
                 var pRating = $("<p>").text("Rating: " + results[i].rating.toUpperCase()); // create p-tag with rating
-                var sportsCarImage = $("<img>"); // create and store img tag
+                var sportsCarImage = $("<img>").addClass("gif mr-4").attr({ // create img tag with class, margin, and attributes
+                    "src": results["images"]["fixed_height_still"]["url"], // original src should be still image
+                    "data-still": results["images"]["fixed_height_still"]["url"], // attr for still image URL
+                    "data-animate": results["images"]["fixed_height"]["url"], // attr for animated image URL
+                    "data-state": "still" // default data-state set to still
+                });
                 
-                var stillImageURL = results[i].images.fixed_height_still.url; // var to hold URL for still image
-                var animatedURL = results[i].images.fixed_height.url; // var to hold URL for animated image
-                sportsCarImage.attr("src", stillImageURL); // set src of img to property from GIPHY API
-                sportsCarImage.attr("data-state", "still"); // give default state of still for on-click to animate or pause
-                sportsCarImage.addClass("gif mr-4"); // add gif class and margin
-
+                // recognize click
+                sportsCarImage.on("click", function(event) {
+                    playPauseGif(event.currentTarget);
+                }
+                
                 sportsCarDiv.append(sportsCarImage); // append img to sportsCarDiv
                 sportsCarDiv.append(pRating); // append rating to sportsCarDiv
 
